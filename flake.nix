12650b4f053d991112cb8c15ba519a44f6ed7d14
Callum Dunster <cdunster@users.noreply.github.com>
2024-11-20 15:46:43 +0100

    chore: add Rust app

--- flake.nix
+++ flake.nix
@@ -2,21 +2,32 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
         };
+        rustFromFile = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in
       {
         devShell = with pkgs; mkShell {
           packages = [
+            rustFromFile
             nomad
           ];
+
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
         };
       });
 }
