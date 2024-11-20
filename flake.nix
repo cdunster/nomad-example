064b1c6f06b8c96ff26586ddcf1bdc29c4dc4aa3
Callum Dunster <cdunster@users.noreply.github.com>
2024-11-20 15:36:46 +0100

    chore: initial commit, add nomad package

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          packages = [
+            nomad
+          ];
+        };
+      });
+}
